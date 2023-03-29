7f13bb017e87c2c2fd7a10e000a887e2ac858773
Itsoon <91259186+Itsoon-xyz@users.noreply.github.com>
2023-03-29 15:41:58 +0200

    update

--- YT-downloader/yt-downloader.py
+++ YT-downloader/yt-downloader.py
@@ -9,7 +9,20 @@ while not validInput:
         validInput = True
     else:
         print("Invalid input. Please enter only youtube URL.")
-# yt = pytube.YouTube(link)
-print(f"Download of {yt.title} in progress please wait...")
+
+
+def downloadProgress(stream, chunk, bytes_remaining):
+    bytes_downloaded = stream.filesize - bytes_remaining
+    percent = bytes_downloaded * 100 / stream.filesize
+    print(percent)
+
+
+yt = pytube.YouTube(link)
+
+yt.register_on_progress_callback(downloadProgress)
+
+print(f"Download of \"{yt.title}\" in progress please wait...")
 # print(yt.streams)
-# yt.streams.get_highest_resolution().download()
+stream = yt.streams.get_highest_resolution()
+stream.download()
+print("The download is done")
