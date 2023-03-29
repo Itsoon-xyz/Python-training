5a3e689e42c417f8eef9e4a4a7362424baf2f4f5
Itsoon <91259186+Itsoon-xyz@users.noreply.github.com>
2023-03-29 12:52:57 +0200

    update

--- YT-downloader/yt-downloader.py
+++ YT-downloader/yt-downloader.py
@@ -1,8 +1,15 @@
 import pytube
+import validators
 
-
-link = input("Enter url: ")
-yt = pytube.YouTube(link)
+validInput = False
+while not validInput:
+    link = input("Enter url: ")
+    validation = validators.url(link)
+    if validation:
+        validInput = True
+    else:
+        print("Invalid input. Please enter only youtube URL.")
+# yt = pytube.YouTube(link)
 print(f"Download of {yt.title} in progress please wait...")
 # print(yt.streams)
-yt.streams.get_highest_resolution().download()
+# yt.streams.get_highest_resolution().download()
